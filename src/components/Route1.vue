2d24fa25e591d4d08f6ed088bfefef9089b1b9e4
Sven12345re <44273129+Sven12345re@users.noreply.github.com>
2020-05-27 13:11:34 +0200

    Firs API Request

--- src/components/Route1.vue
+++ src/components/Route1.vue
@@ -1,83 +1,31 @@
 <template>
-    <v-app id="inspire">
-        <v-navigation-drawer
-                v-model="drawer"
-                app
-                clipped
-        >
-            <v-list dense>
-                <v-list-item link>
-                    <v-list-item-action>
-                        <v-icon>mdi-view-dashboard</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                        <v-list-item-title>Dashboard</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-                <v-list-item link>
-                    <v-list-item-action>
-                        <v-icon>mdi-settings</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                        <v-list-item-title>Settings</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-            </v-list>
-        </v-navigation-drawer>
-
-        <v-app-bar
-                app
-                clipped-left
-        >
-            <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-            <v-toolbar-title>Application</v-toolbar-title>
-        </v-app-bar>
-
-        <v-content>
-            <v-container
-                    class="fill-height"
-                    fluid
-            >
-                <v-row
-                        align="center"
-                        justify="center"
-                >
-                    <v-col class="shrink">
-                        <v-tooltip right>
-                            <template v-slot:activator="{ on }">
-                                <v-btn
-                                        :href="source"
-                                        icon
-                                        large
-                                        target="_blank"
-                                        v-on="on"
-                                >
-                                    <v-icon large>mdi-code-tags</v-icon>
-                                </v-btn>
-                            </template>
-                            <span>Source</span>
-                        </v-tooltip>
-                    </v-col>
-                </v-row>
-            </v-container>
-        </v-content>
-
-        <v-footer app>
-            <span>&copy; 2020</span>
-        </v-footer>
-    </v-app>
+    <div>{{bloggerData.name}}</div>
 </template>
 
 <script>
     export default {
-        props: {
-            source: String,
+
+        data(){
+            return{
+                bloggerData: null
+            }
         },
-        data: () => ({
-            drawer: null,
-        }),
-        created () {
-            this.$vuetify.theme.dark = true
+        mounted(){
+            var vm = this
+            vm.fetchBloggerApi()
         },
+        methods:{
+            fetchBloggerApi(){
+                var vm = this;
+                const axios = require('axios');
+                axios.get('https://www.googleapis.com/blogger/v3/blogs/1638418377018710180?key=AIzaSyBjTdt2MEa14lzK0EakTkL4pgRCNg4I7zc')
+                    .then(function (response) {
+                        vm.bloggerData = response.data
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+            }
+        }
     }
 </script>
